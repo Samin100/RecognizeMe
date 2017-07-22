995d648c70292b2f1ee127e7009776d97aa8a3fb
asmita21 <asmita21@vt.edu>
2017-07-22 02:36:28 -0400

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,10 +1,14 @@
+
+
 import cv2
 image = cv2.imread("clouds.jpg")
 vidcap = cv2.VideoCapture("security_cam_feed.mov");
+
 count = 0
 while vidcap.isOpened():
     ret,frame = vidcap.read()
-    cv2.imshow('window-name',frame)
+    resized_image = cv2.resize(frame, (1400,800))
+    cv2.imshow('window-name',resized_image)
     cv2.imwrite("frame%d.jpg" % count, frame)
     count = count + 1
     if cv2.waitKey(10) & 0xFF == ord('q'):
